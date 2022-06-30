87b4addf32b3c61306c71ccb3e5eb00123cbc9f7
Brian <brianbtfa@hotmail.com>
2022-06-29 19:52:47 -0500

    deleting lib

--- lib/Register/ui/widget/logo_sign_up.dart
+++
@@ -1,8 +0,0 @@
-  import 'package:flutter/material.dart';
-
-SizedBox logoSignUp(double width) {
-    return SizedBox(
-            width: width * 0.6,
-            child: Image.asset('assets/LOGO_PETFIND.png'),
-          );
-  }
