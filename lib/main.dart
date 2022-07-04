0c8dfd766d0d75fa351bb896e27bd72704ba84c8
Brian <brianbtfa@hotmail.com>
2022-07-04 10:02:38 -0500

    corrigiendo vista de register y agregando boton

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:petfind/Home/ui/screen/home_main.dart';
 import 'package:petfind/Login/ui/screen/login.dart';
 import 'package:petfind/Register/ui/screen/sign_up.dart';
 import 'package:petfind/RegisterPet/ui/screen/pet_data_register.dart';
 import 'package:petfind/RegisterPet/ui/screen/pet_image.dart';
 import 'package:petfind/RegisterPet/ui/screen/register_pet_main.dart';
-
 
 void main() {
   runApp(const MyApp());
@@ -17,6 +17,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
@@ -24,7 +28,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         fontFamily: 'Chau Philomene One',
       ),
-      initialRoute: '/',
+      initialRoute: '/petRegisterImage',
       routes: {
         '/': (context) => Login(),
         '/register_page': (context) => CreateAccount(),
